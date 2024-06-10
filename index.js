b7d1941abbdb367bee3fbaab878c790037d6e44f
MOA -CODES <moyo.ajla@gmail.com>
2024-06-10 12:45:58 +0100

    deployed to vercel1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 require('dotenv').config() 
 require('express-async-errors')
 
+//docs start
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+
+const path = require('path')
+const swaggerPath = path.resolve(__dirname, './swagger.yaml')
+const swaggerDoc = YAML.load(swaggerPath)
+const swaggerCss = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css"
+
+//docs end
+
 const cors = require('cors')
 const express = require('express')
 
@@ -39,11 +50,18 @@ const port = process.env.PORT || 3001
 app.use(cors(corsOptions))
 
 app.get('/', (req,res)=>{
-    res.send('Kannel Configuration Service')
+    res.send('<center>\
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"\
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"crossorigin="anonymous" />\
+    <h1>Kannel Configuration Service</h1>\
+    <h3><p><b>Made by <i><a href="https://github.com/MOA-CODES">MOA-CODES</a></i></b></P></h3>\
+    </center>')
    }
 )
 
 app.post('/api/v1/writeToKannel', writeToKannelHandler)
+app.get('/api/v1/writeToKannel', writeToKannelHandler)
+
 
 app.listen(port, ()=>{
     console.log(`listening on ${port}`)
